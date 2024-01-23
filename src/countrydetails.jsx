1399e7d088b77bc0281087bd6e9bed69e06167d7
sanjeeth-singh <sanjeethsinghthakur0090@gmail.com>
2024-01-23 21:31:07 +0530

    params

--- src/countrydetails.jsx
+++ src/countrydetails.jsx
@@ -5,7 +5,6 @@ function Countrydetails({country}){
         <div>
             {country?.name?.common}
             <img src={country?.flags[0]} alt="" width={"100%"}/>
-            <img src={country?.flags[0]} alt="" width={"100%"}/>
         </div>
     )
 }
